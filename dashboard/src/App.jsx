6dd8b7fd414e68d870a30eb500ffaf802c8dd818
ARJUN AGARWAL <123585663+sahhoArjun097@users.noreply.github.com>
2025-01-25 22:46:15 +0530

    project begin

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <div className='w-[100px] bg-amber-400'>App</div>
+  )
+}
+
+export default App
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div>App</div>
+    <div className="w-full h-full flex flex-col items-center bg-gray-100">
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        {/* <Route path="" element={}/> */}
+      </Routes>
+    </Router>
+    </div>
   )
 }
 
-export default App
+export default App;
