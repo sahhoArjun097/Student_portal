91597d94ceb3b6012695d90700db833799cfcb24
ARJUN AGARWAL <123585663+sahhoArjun097@users.noreply.github.com>
2025-02-12 00:15:48 +0530

    addde the isadminauth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -11,7 +11,7 @@ export  const  isAdminAuthenticated = catchAsyncError(async( req , res , next)=>
     }
     const  decoded = jwt.verify(token,config.JWT_SECRET_KEY);
     req.user = await User.findById(decoded.id);
-    if(res.user.role!=="admin" ){
+    if(req.user.role!=="admin" ){
         return next(new ErrorHandler(`${req.user.role} not authorized to this resource`,403 ))
     }
     next()
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {  Admin, Login, Logout, StudentRegister, TeacherRegister } from "../controller/userController.js";
+import { isAdminAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/student/register",StudentRegister)
-router.post("/teacher/register",TeacherRegister)
+router.post("/student/register",isAdminAuthenticated,StudentRegister)
+router.post("/teacher/register",isAdminAuthenticated,TeacherRegister)
 router.post("/login",Login);
 router.post("/admin",Admin);
 router.get("/logout",Logout);
